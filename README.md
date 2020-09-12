ab60b105b1198dae97976b320c1fd343bed1d460
Harrison Caudill <harrison@hypersphere.org>
2020-09-12 00:24:07 -0700

    add an example private.py file and update the readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# ipo-sim
+ * have grants and sell orders, but should transition to starting
+   grants, sell orders, and ending grants.  that allows computing of
+   final position, and maintains cache coherency.
+
+ * Does not do anything with long-term capital gains
+
+ * The tax tables and standard deductions assume you are a married
+   couple filing jointly
+
+ * you MUST MUST MUST run `model.clear_cache()` after you execute the
+   `sell` method in one of the `Grant` objects because it will
+   invalidate the cache, but pylink won't know...yeah yeah yeah, I
+   need to add a `cache_invalidate(node)` routine to the DAGModel in
+   pylink....get off my back.
+
+ * unit tests for selling
+
+ * unit tests for income tracking
+
+ * need to do a meticulous walkthrough on the unit tests in general
+
+ * There can be some weirdness regarding around the number of options
+   -- adjust using the cliff_n as needed.
+
+ * 
---
+++ eg_private.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import math
+import pylink
+
+import income as myMeager
+import position as anAwkward
+import taxes as deathAnd
+
+
+# cuz I'm lazy
+from position import Grant
+
+GRANTS = [
+    Grant(name='rsu',
+          vehicle='rsu',
+          strike_usd=4,
+          start='1/1/20',
+          n_periods=12,
+          n=1<<0,
+          period_months=1),
+
+    Grant(name='nso',
+          vehicle='nso',
+          strike_usd=4,
+          start='1/1/20',
+          n_periods=12,
+          n=1<<1,
+          exercised=0,
+          sold=0,
+          period_months=1),
+
+    Grant(name='iso',
+          vehicle='iso',
+          strike_usd=4,
+          start='1/1/20',
+          n_periods=12,
+          n=1<<2,
+          exercised=0,
+          sold=0,
+          period_months=1),
+    ]
+
+constants = {
+    'ipo_price_usd': 12,
+    'query_date': '9/23/20',
+
+    'palantir_401k_usd': 0,
+    'palantir_fsa_usd': 0,
+    'palantir_drca_usd': 0,
+    'reg_income_usd': 0,
+    'ext_amt_income_usd': 0,
+    'iso_exercise_income_usd': 0,
+    'fed_withheld_usd': 0,
+    'state_withheld_usd': 0,
+    }
+MODEL = pylink.DAGModel([deathAnd.Taxes(),
+                         myMeager.Income(),
+                         anAwkward.Position(GRANTS)],
+                        **constants)
