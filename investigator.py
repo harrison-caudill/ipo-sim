831f3b1288173941d03c4dc8fe4cd84e1b972e78
Harrison Caudill <harrison@hypersphere.org>
2020-09-12 14:25:53 -0700

    break out the questions

--- investigator.py
+++ investigator.py
@@ -85,51 +85,59 @@ class Investigator(object):
         e = m.enum
         m.override(e.sales_orders, orders)
 
-    def go(self):
+    def question_1(self):
         self._qst(1, "How many RSUs will be automatically withheld?")
         held = m.shares_withheld_rsu_n
         vested = m.shares_vested_rsu_n
         rate = held / float(vested)
         print("Withholding %d / %d (%.2f)" % (held, vested, rate))
 
+    def question_2(self):
         self._qst(2, "What is the outstanding tax burden?")
         rep = explosive.Report(m)
         rep.print_tax_summary()
 
-        self._qst(3, "How many RSUs need to be sold to cover tax burden?")
+    def owed(self):
+        m = self.m
         owed = ( 0.0
                  + m.tax_burden_usd
                  - m.fed_withheld_usd
                  - m.state_withheld_usd
                  - m.shares_withheld_rsu_usd
                  + 0.0 )
-        needed = int(math.ceil(owed / float(m.ipo_price_usd)))
+        return owed
+
+    def question_3(self):
+        self._qst(3, "How many RSUs need to be sold to cover tax burden?")
+        needed = int(math.ceil(self.owed() / float(m.ipo_price_usd)))
         print("Share Price: %.2f" % m.ipo_price_usd)
-        print("Owed:    %s" % explosive.comma(owed))
+        print("Owed:    %s" % explosive.comma(self.owed()))
         pct = int((needed * 100) / m.shares_vested_rsu_n)
         print("Sell:        %d (%d %%)" % (needed, pct))
 
+    def question_4(self):
         self._qst(4, "How much cash if we sell it all (start with NSO)?")
         self.sell_it_all(m)
         cleared = ( 0.0
                     + m.total_income_usd
                     - m.reg_income_usd
-                    - owed
+                    - self.owed()
                     - m.shares_withheld_rsu_usd
                     + 0.0 )
         print("We Clear: %s"%explosive.comma(cleared))
 
+    def question_5(self):
         self._qst(5, "How much cash if we sell the RSUs?")
         self.sell_rsu(m)
         cleared = ( 0.0
                     + m.total_income_usd
                     - m.reg_income_usd
-                    - owed
+                    - self.owed()
                     - m.shares_withheld_rsu_usd
                     + 0.0 )
         print("We Clear: %s"%explosive.comma(cleared))
 
-
+    def question_6(self):
         self._qst(6, "If we sell it all, how many ISOs can we buy w/o AMT?")
         self.sell_it_all(m)
 
@@ -149,7 +157,7 @@ class Investigator(object):
         cleared = ( 0.0
                     + m.total_income_usd
                     - m.reg_income_usd
-                    - owed
+                    - self.owed()
                     - m.shares_withheld_rsu_usd
                     + 0.0 )
         remaining = cleared - cost
@@ -160,7 +168,7 @@ class Investigator(object):
         print("Cash Cleared:    %s" % explosive.comma(float(cleared)))
         print("Cash Remaining:  %s" % explosive.comma(float(remaining)))
 
-
+    def question_7(self):
         self._qst(7, "If we sell all RSUs, how many ISOs can we buy w/o AMT?")
         self.sell_rsu(m)
 
@@ -181,7 +189,7 @@ class Investigator(object):
         cleared = ( 0.0
                     + m.total_income_usd
                     - m.reg_income_usd
-                    - owed
+                    - self.owed()
                     - m.shares_withheld_rsu_usd
                     + 0.0 )
         remaining = cleared - cost
@@ -193,9 +201,21 @@ class Investigator(object):
         print("Cash Remaining:  %s" % explosive.comma(float(remaining)))
         print()
 
+    def question_8(self):
         self._qst(8, "What does our remaining position look like?")
+        rep = explosive.Report(m)
         rep.print_grants()
 
+    def go(self):
+        self.question_1()
+        self.question_2()
+        self.question_3()
+        self.question_4()
+        self.question_5()
+        self.question_6()
+        self.question_7()
+        self.question_8()
+        
 if __name__ == '__main__':
     m = private.MODEL
     dix = Investigator(m)
